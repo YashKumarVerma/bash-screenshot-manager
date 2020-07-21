73023fb1d804e611993a123f207de1b63d7b1293
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-07-22 01:28:49 +0530

    pre-alpha

--- screenshot-manager.sh
+++ screenshot-manager.sh
@@ -9,17 +9,11 @@
 PREFIX="      | "
 # the following variable changes the location of the folder which stores the screenshots
 STORAGE_FOLDER="college-screenshots"
-#
+# folder name format
+DATE=$(date +"%B %d %G - %A")
 #
 #
 # Your configurations end here. Time to use it 
-
-
-function template( ) { 
-    echo "$PREFIX [Started] xyz"
-    echo "$PREFIX [Finished] xyz"
-}
-
 
 # function to show usage
 function show_usage() {
@@ -29,7 +23,6 @@ function show_usage() {
     echo "$PREFIX $0 save dsa       => save DSA class screenshots"
     echo "$PREFIX $0 save dsa --lab => save DSA lab screenshots"
 }
-
 
 # check if directory exists
 echo "$PREFIX"
@@ -42,9 +35,35 @@ else
 fi
 echo "$PREFIX"
 
-#  command router 
+# check if subject name passed or not. if not, then end the session 
 if [[ -z $1 ]]; then
     echo "$PREFIX No arguments passed."
     show_usage
     exit 1
 fi
+
+
+# generate separate but attached folder for lab
+TARGET_PATH="$STORAGE_FOLDER/$1/$DATE"
+if [[ "$2" = "--lab" ]]; then
+    echo "$PREFIX Processing as lab class"
+    TARGET_PATH="$STORAGE_FOLDER/$1-lab/$DATE"
+fi
+
+# if user reaches here, it means that they have entered the subject
+if [[ -e "$TARGET_PATH" ]] ;
+then 
+    # if foldr exists, check if today's date folder exist
+    echo "$PREFIX Screenshots of this class are already present."
+    echo "$PREFIX Running again, nothing is being deleted. chill"
+else
+    mkdir -p "$TARGET_PATH"
+    echo "$PREFIX folder for screenshots of this class not found"
+    echo "$PREFIX folder for screenshots of this class created"
+fi
+
+
+# if user has reached here, it means the desired folder is created till now, time to move the contnet
+mv *.jpg "$TARGET_PATH"
+
+echo "$PREFIX Done"
