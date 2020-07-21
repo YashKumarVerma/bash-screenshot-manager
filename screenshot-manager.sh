6230269fb1bc819d5a92d142de79e40cd30ac0ba
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-07-22 00:52:44 +0530

    remove unwanted condition checks

--- screenshot-manager.sh
+++ screenshot-manager.sh
@@ -24,31 +24,27 @@ function template( ) {
 # function to show usage
 function show_usage() {
     echo "$PREFIX"
-    echo "$PREFIX $0 help   => list this menu"
-    echo "$PREFIX $0 show   => list the current screenshot"
+    echo "$PREFIX $0 help           => list this menu"
+    echo "$PREFIX $0 show           => list the current screenshot"
+    echo "$PREFIX $0 save dsa       => save DSA class screenshots"
+    echo "$PREFIX $0 save dsa --lab => save DSA lab screenshots"
 }
 
 
 # check if directory exists
+echo "$PREFIX"
 if [[ -e "$STORAGE_FOLDER" ]] ;
 then 
-    echo "$PREFIX Old screenshot folder found"
+    echo "$PREFIX Using old screenshot folder"
 else
     echo "$PREFIX Old screenshot folder not found, created"
     mkdir $STORAGE_FOLDER
 fi
-
+echo "$PREFIX"
 
 #  command router 
 if [[ -z $1 ]]; then
     echo "$PREFIX No arguments passed."
     show_usage
-
-# show help commands
-elif [[ "$1" = "help" ]]; 
-    then show_usage
-
-# default message
-else echo "$PREFIX Select a Valid choice from "
-    show_usage
+    exit 1
 fi
